49c43b4965d8a9519f25c943047795dbee57e911
hawonjungo <hawon.jungo@gmail.com>
2022-11-02 13:38:27 +1100

    Fix WebRouter to be a component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 
 // import WebRouter from "./components/WebRouter";
 import WebRouter from "./components/knowRouter/WebRouter";
 
 const App = () => {
   return (
-    //BrowserRouter working as Routing mechanism for whole website ( if wrap them all)
     <div>
-      <BrowserRouter>
-        <WebRouter />
-      </BrowserRouter>
+      <WebRouter />
     </div>
   );
 };
